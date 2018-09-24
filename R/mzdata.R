6530edeb2fed8aacb2bc43045a91079b8a55c64b
Benjamin R. Gordon <brgordon17@gmail.com>
2018-09-25 01:30:24 +1000

    modifed to include timepoint 1 and extra PBQCs. changed some subsetting to use dplyr. Added a batch cat variable. Have run the script by line and saved mzdata.rda to ./data. Has not been tested as a fucntion as yet.

--- R/mzdata.R
+++ R/mzdata.R
@@ -1,13 +1,13 @@
 #' Create mzdata.
 #'
 #' \code{create_mzdata()} pre-processes the LCMS data used for modelling in
-#' gordon01.
+#' gordon-C5.
 #'
 #' Initially, the function takes the raw output from xcms and removes unwanted
 #' data (e.g. retention times, isotopes, peak counts etc.). Then, it creates
 #' new categorical variables based on the sample information. Finally, it
 #' replaces true non-detects with noise, removes poorly resolved mass features
-#' and then replaces the small number of remining missing values using random
+#' and then replaces the small number of remaining missing values using random
 #' forest imputaion. The list below details the logic behind the missing values
 #' imputation:
 #' \itemize{
@@ -52,31 +52,36 @@
 #' @export
 #'
 create_mzdata <- function(parallel = FALSE,
-                          seed = 100,
+                          seed = 1978,
                           savecsv = FALSE,
                           saverda = TRUE,
                           csvname = "mzdata") {
   
   mzdata  <-  readr::read_csv("./data-raw/mzdata-raw.csv", na = "0")
   
-  # remove unwanted data -------------------------------------------------------
+  # remove isotopes ------------------------------------------------------------
   mzdata <- mzdata[-grep("[M+1]", mzdata$isotopes, fixed = TRUE),]
   mzdata <- mzdata[-grep("[M+2]", mzdata$isotopes, fixed = TRUE),]
   mzdata <- mzdata[-grep("[M+3]", mzdata$isotopes, fixed = TRUE),]
   mzdata <- mzdata[-grep("[M+4]", mzdata$isotopes, fixed = TRUE),]
-  mzdata <- mzdata[, -133:-135]
+  
+  # Clean up -------------------------------------------------------------------
+  mzdata <- dplyr::select(mzdata, -isotopes, -adduct, -pcgroup)
+  mzdata <- mzdata[-2:-20]
   mz_names <- round(mzdata[, 1], 4)
-  mzdata <- mzdata[, -1]
   mz_names <- paste("mz", mz_names$mz, sep = "_")
   mz_names <- make.names(mz_names, unique = TRUE)
   mzdata <- tibble::as_tibble(t(mzdata), rownames = "sample_ids")
   colnames(mzdata)[2:ncol(mzdata)] <- mz_names
-  sample_ids <- mzdata[1]
+  mzdata <- mzdata[-1, ]
+  sample_ids <- mzdata$sample_ids
   
   # Create categorical variables -----------------------------------------------
-  class <- c(rep("PBQC", 11), 
+  class <- c(rep("PBQC", 18), 
              rep("cont", 12), 
              rep("treat", 12), 
+             rep("cont", 12), 
+             rep("treat", 9),
              rep("cont", 12), 
              rep("treat", 12),
              rep("cont", 12), 
@@ -88,27 +93,42 @@ create_mzdata <- function(parallel = FALSE,
   class <- factor(class, levels = c("cont", "treat", 
                                     "PBQC"))
   
-  day <- c(rep("PBQC", 11),
-           rep("day5", 24),
+  day <- c(rep("PBQC", 18),
+           rep("day0", 24),
+           rep("day5", 21),
            rep("day8", 24),
            rep("day10", 24),
            rep("day12", 24),
-           rep("day15", 24))
+           rep("day16", 24))
   day <- factor(day,
-                levels = c("day5", "day8", "day10", "day12", "day15", 
+                levels = c("day0", "day5", "day8", "day10", "day12", "day16", 
                            "PBQC"))
   
-  tank <- c(rep("PBQC", 11),
+  tank <- c(rep("PBQC", 18),
+            c("02",  "02",  "02",  "13",  "13",  "13",  "40",  "40", "40", 
+              "48",  "48", "48",  "20",  "20",  "20",  "25", "25",  "25", 
+              "54", "54",  "54",  "55", "55", "55"),
+            c("02",  "02",  "02",  "13",  "13",  "13",  "40",  "40", "40", 
+              "48",  "48", "48",  "25", "25",  "25", "54", "54",  "54", 
+              "55", "55", "55"),
             rep(c("02",  "02",  "02",  "13",  "13",  "13",  "40",  "40", "40", 
                   "48",  "48", "48",  "20",  "20",  "20",  "25", "25",  "25", 
-                  "54", "54",  "54",  "55", "55", "55"), 5))
+                  "54", "54",  "54",  "55", "55", "55"), 4))
   tank <- factor(tank, 
                  levels = c("02", "13", "40", "48", "20", "25", "54", "55", 
                             "PBQC"))
   
-  rep <- c(c(1, 10, 11, 2:9), 
-           rep(c(1, 2, 3), 40))
+  rep <- c(c(1, 1, 10, 11, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 9), 
+           rep(c(1, 2, 3), 47))
   rep <- factor(rep, levels = (c(1:11)))
+  
+  batch <- c(1, 2, 2, 2, rep(c(1, 2), 6), 2, 2,
+             rep(2, 24), 
+             rep(1, 21),
+             rep(2, 24),
+             rep(2, 24),
+             rep(1, 24),
+             rep(2, 24))
 
   class_day <- interaction(class,
                            day,
@@ -127,6 +147,7 @@ create_mzdata <- function(parallel = FALSE,
                        day,
                        tank,
                        rep,
+                       batch,
                        mzdata_filt$output)
   percent_na <- round(sum(is.na(mzdata))/prod(dim(mzdata[-1:-6]))*100, 2)
   
@@ -146,7 +167,7 @@ create_mzdata <- function(parallel = FALSE,
   mzdata <- dplyr::arrange(mzdata, class, day)
   
   # write data -----------------------------------------------------------------
-  #save(mzdata, file = "./data/mzdata.rda", compress = "bzip2")
+  save(mzdata, file = "./data/mzdata.rda", compress = "bzip2")
   
   if(saverda) {
     devtools::use_data(mzdata)
