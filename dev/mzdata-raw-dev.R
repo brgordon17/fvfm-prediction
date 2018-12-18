ebce921d6aac6ed22a0528c18b5239217611a3c5
Benjamin R. Gordon <brgordon17@gmail.com>
2018-12-18 15:55:08 +1000

    finshed script development. Need to examine how to add CAMERA integration for detecting isotopes and adducts etc.

--- dev/mzdata-raw-dev.R
+++ dev/mzdata-raw-dev.R
@@ -36,11 +36,12 @@ boxplot(tics, col = class_colours[raw_data$class],
 
 # peak inspection --------------------------------------------------------------
 
+# NOTE: THis step is not useful without known compounds or internal standards.
 # check peak width by selecting a peak that is common to all samples as IDd in 
 # the BPC and mMass.app
 # define rt range
 rtr <- c(20, 100)
-mzr <- c(135.1, 136.9)
+mzr <- c(136.05, 136.07)
 # extract chromatogram
 chr_raw <- chromatogram(raw_data, rt = rtr, mz = mzr)
 plot(chr_raw, col = class_colours[raw_data$class])
@@ -53,30 +54,92 @@ raw_data %>%
 
 # Peak detection ---------------------------------------------------------------
 
-# define the parameter used (centwave here)
+# define the parameters used for peak detection (centwave here)
 cwp <- CentWaveParam(ppm = 30, 
-                     peakwidth = c(20, 60),
+                     peakwidth = c(20, 50),
+                     snthresh = 10,
+                     prefilter = c(3, 100),
+                     mzCenterFun = "wMean",
+                     integrate = 1L,
+                     mzdiff = -0.001,
+                     fitgauss = FALSE,
                      noise = 1000)
+
+# Do peak detection
 mzdata <- findChromPeaks(raw_data, param = cwp)
 
-# view peak counts
-summary_fun <- function(z) {
-  c(peak_count = nrow(z), rt = quantile(z[, "rtmax"] - z[, "rtmin"]))
-}
-peak_summary <- lapply(split.data.frame(chromPeaks(mzdata),
-                             f = chromPeaks(mzdata)[, "sample"]),
-            FUN = summary_fun)
-peak_summary <- do.call(rbind, peak_summary)
-rownames(peak_summary) <- basename(fileNames(mzdata))
-View(peak_summary)
+# view peaks given an mz and rt range
+chromPeaks(mzdata, mz = c(136, 137), rt = rtr)
 
-# ensure previously examined peak was picked correctly
-plot(chr_raw, col = class_colours[raw_data$class], lwd = 2)
-highlightChromPeaks(mzdata, 
-                    border = class_colours[raw_data$class],
-                    lty = 3,
-                    rt = rtr,
-                    mz = mzr)
+# view peaks given an mz and a ppm error
+chromPeaks(mzdata, mz = 136.1076, ppm = 10 )
+
+# Alignment --------------------------------------------------------------------
+
+mzdata <- adjustRtime(mzdata, param = ObiwarpParam(binSize = 0.5))
+
+# view unadjusted retention times
+head(rtime(mzdata, adjusted = FALSE))
+
+# and adjusted
+head(rtime(mzdata))
+
+# compare adjusted BPCs with unadjusted
+# original
+bpcs <- chromatogram(raw_data, aggregationFun = "max")
+class_colours <- c("grey80", "red2")
+names(class_colours) <- c("eCO2eT", "recovery")
+plot(bpcs, col = class_colours[raw_data$class])
+
+# adjusted
+bpcs_adj <- chromatogram(mzdata, aggregationFun = "max")
+plot(bpcs_adj, col = class_colours[raw_data$class])
+
+# plot retention time adjustment
+plotAdjustedRtime(mzdata, col = class_colours[mzdata$class])
+
+# Correspondence or Grouping ---------------------------------------------------
+
+# define mz range
+mzr <- c(138.0, 138.1)
+
+# Extract and plot the chromatograms
+chr_mzr <- chromatogram(mzdata, mz = mzr, rt = c(20, 300))
+cols <- class_colours[chr_mzr$class]
+plot(chr_mzr, col = cols)
+
+# Highlight the detected peaks in that region.
+highlightChromPeaks(mzdata, mz = mzr, col = cols, type = "point", pch = 16)
+
+# Define the parameters for the peak density method
+pdp <- PeakDensityParam(sampleGroups = mzdata$class,
+                        minFraction = 0.4, bw = 30)
+plotChromPeakDensity(mzdata, mz = mzr, col = cols, param = pdp,
+                     pch = 16, xlim = c(20, 100))
+
+# Use a different bw
+pdp <- PeakDensityParam(sampleGroups = mzdata$class,
+                        minFraction = 0.4, bw = 5)
+plotChromPeakDensity(mzdata, mz = mzr, col = cols, param = pdp,
+                     pch = 16, xlim = c(20, 100))
+
+# perform grouping
+pdp <- PeakDensityParam(mzdata$class,
+                        bw = 5,
+                        minFraction = 0.5,
+                        binSize = 0.05)
+mzdata <- groupChromPeaks(mzdata, param = pdp)
+
+# quick look at the results
+featureDefinitions(mzdata)
+
+# fill in missing peaks
+mzdata <- fillChromPeaks(mzdata)
+
+# get data
+
+
+
 
 
 
