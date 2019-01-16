92da981706b4be915301a96d1578be3392a89640
Benjamin R. Gordon <brgordon17@gmail.com>
2019-01-16 14:06:17 +1000

    added commands to use in shell. Will change name to shell_commands.R

--- R/test_file.R
+++ R/test_file.R
@@ -1,5 +1,12 @@
 # Test file
 
-# Changing filenames while keeping git record.
+# Changing filenames while keeping git record ----------------------------------
 # In a shell (terminal)
-# git mv oldfilename newfilename
+ git mv oldfilename newfilename
+
+# Common shell commands --------------------------------------------------------
+pwd # print working dir
+ls # list files
+cd # change dir
+
+
