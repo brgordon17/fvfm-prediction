d05bf82ac96f6b67e8bd1759a24d7717073fecf1
Benjamin R. Gordon <brgordon17@gmail.com>
2019-02-26 00:02:43 +1000

    Changed plot to include all days of the experiment using ./data/pamdata.rda.

--- R/thesis_figures.R
+++ R/thesis_figures.R
@@ -5,27 +5,38 @@
 
 library(tidyverse)
 
-# PSII FvFm line plot ----------------------------------------------------------
+# PSII FvFm plot ---------------------------------------------------------------
 
 # load data
-load("./data/metadata.rda")
-metadata <- filter(metadata, cont_treat != "PBQC")
-metadata$day <- factor(metadata$day,
-                       levels = c("day 1", "day 5", "day 8", "day 10", "day 12",
-                                  "day 15"))
+load("./data/pamdata.rda")
+
+# summarise data
+sum_pamdata <- 
+  pamdata %>% 
+  group_by(class, day) %>%
+  summarise(mean = mean(yield), 
+            sd = sd(yield), 
+            se = sd(yield)/sqrt(n())
+            )
 
 # create plot
-ggplot(metadata, aes(x = day,
-                     y = FvFm,
-                     colour = cont_treat,
-                     group = cont_treat)) +
-  geom_line() +
-  geom_point(size = 2) +
-  scale_x_discrete(name = "Day") +
-  scale_y_continuous(name = "Quantum Yield (FvFm)") +
+ggplot(sum_pamdata, aes(x = day,
+                        y = mean,
+                        colour = class,
+                        shape = class)) +
+  geom_errorbar(aes(ymin = mean - se, ymax = mean + se), 
+                width = .1,
+                show.legend = FALSE) +
+  geom_point(size = 3, show.legend = TRUE) +
+  scale_x_discrete(name = "Time (days)",
+                   labels = c(1:15)) +
+  scale_y_continuous(name = expression(Quantum~Yield~PSII~(F[v]/F[m]))) +
   scale_colour_manual(values = gordon01::qual_colours, 
                       name = NULL,
-                      labels = c("control", "treatment")) +
+                      labels = c("control", "heated")) +
+  scale_shape_manual(values = c(16, 17),
+                     name = NULL,
+                     labels = c("control", "heated")) +
   theme(panel.background = element_blank(),
         panel.grid.major.x = element_blank(),
         panel.grid.major.y = element_line(colour = "grey90",
@@ -33,18 +44,10 @@ ggplot(metadata, aes(x = day,
         axis.text = element_text(size = 10, colour = "grey65"),
         axis.title = element_text(size = 12, colour = "grey65"),
         axis.ticks = element_blank(),
-        legend.key = element_blank()
+        legend.key = element_blank(),
+        legend.text = element_text(size = 12)
         )
 ggsave("./figs/fvfm_plot.pdf",
        width = 10,
        height = 6.5,
        units = "in")
-
-
-
-
-
-
-
-
-
