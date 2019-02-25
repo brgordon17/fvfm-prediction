ebff0f964efcfb631432f33099ad0b63d5cc8df6
Benjamin R. Gordon <brgordon17@gmail.com>
2019-02-25 22:19:10 +1000

    Done. This appears to be correct after a fair bit of research. For this experiment a RMANOVA with Tukey's HSD is most appropriate.

--- R/fvfm_anova.R
+++ R/fvfm_anova.R
@@ -1,29 +1,36 @@
-# Script to run anovas on PSII yield
+# Script to run RM ANOVA and post-hoc analysis on PSII yield
 
 library(tidyverse)
+library(car)
 
 # load data
 load("./data/pamdata.rda")
 
-# students t-test
-pam_tests <- list(
-  "day_1" = t.test(yield ~ class,
-                   data = filter(pamdata, day == "day 1")),
-  "day_5" = t.test(yield ~ class,
-                   data = filter(pamdata, day == "day 5")),
-  "day_8" = t.test(yield ~ class,
-                   data = filter(pamdata, day == "day 8")),
-  "day_10" = t.test(yield ~ class,
-                    data = filter(pamdata, day == "day 10")),
-  "day_12" = t.test(yield ~ class,
-                    data = filter(pamdata, day == "day 12")),
-  "day_15" = t.test(yield ~ class,
-                    data = filter(pamdata, day == "day 15"))
-  )
-str(pam_tests)
+# summary stats
+summary(pamdata$yield[pamdata$class == "control"])
+summary(pamdata$yield[pamdata$class == "treatment"])
 
-# construct df
-pam_tests <- data.frame(pval = sapply(pam_tests, "[[", "p.value"),
-                        t_stat = sapply(pam_tests, "[[", "statistic"),
-                        df = sapply(pam_tests, "[[", "parameter")
-                        )
+# Rm ANOVA
+pam_aov <- aov(yield ~ class * day, data = pamdata)
+summary(pam_aov)
+# report as (RMANOVA, F -df_resid, -df_class:day = Fval, p<0.001)
+
+# check the residuals are normally distributed
+res <- pam_aov$residuals
+hist(res, main = "Histogram of residuals", xlab = "Residuals")
+
+# check for equality of variance
+# Use lower p threshold if equality of variance isnt true (p < 0.05)
+leveneTest(yield ~ class, data = pamdata)
+
+# Tukey hsd to see which groups differ
+TukeyHSD(pam_aov)
+
+# pull out control vs treatment for each sampling point
+pam_tukey <- as_tibble(TukeyHSD(pam_aov)$`class:day`, rownames = "interaction")
+pam_tukey <- filter(pam_tukey, interaction == "treatment:day 1-control:day 1" |
+                      interaction == "treatment:day 5-control:day 5" |
+                      interaction == "treatment:day 8-control:day 8" |
+                      interaction == "treatment:day 10-control:day 10" |
+                      interaction == "treatment:day 12-control:day 12" |
+                      interaction == "treatment:day 15-control:day 15")
