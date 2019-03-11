1f5568c1441556eeffc28c2d2a6e42e524218975
Benjamin R. Gordon <brgordon17@gmail.com>
2019-03-11 22:22:48 +1000

    Initial commit

---
+++ R/tempdata.R
@@ -0,0 +1,9 @@
+# Script to create tempdata.rda
+# Author: Benjamin R. Gordon
+# Date: 2019-03-11
+
+library(tidyverse)
+
+# load data
+tempdata <- 
+  read_csv("~/Documents/Experiment Data/Heron Bleaching_May2013/Temp Data/Temp Data_HeronMay13.csv")
