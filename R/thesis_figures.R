f3ac6581d6ffb899979c7c839edc96fe74df4b05
Benjamin R. Gordon <brgordon17@gmail.com>
2019-03-12 00:42:38 +1000

    added code for temperature plot. swapped the colours around in the fvfm plot to be the same as temp.

--- R/thesis_figures.R
+++ R/thesis_figures.R
@@ -3,6 +3,46 @@
 # Author: Benjamin R. Gordon
 # Date: 21-FEB-2019
 
+# temperature profile plot -----------------------------------------------------
+library(tidyverse)
+
+# load data
+load("./data/tempdata.rda")
+
+plot_time <- c(as.POSIXct("2013-05-08", format = "%F"), 
+               as.POSIXct("2013-05-22", format = "%F"))
+
+# Plot
+ggplot(data = tempdata, aes(x = time)) +
+  geom_path(aes(y = meanC, colour = "Control")) +
+  geom_path(aes(y = meanT, colour = "Heated")) +
+  geom_path(aes(y = meanIMOS, colour = "Reef")) +
+  labs(title = NULL, x = "Time (days)", y = "Temperature"~(degree~C)) +
+  scale_color_manual(values = c("Control" = gordon01::qual_colours[2],
+                                "Heated" = gordon01::qual_colours[1],
+                                "Reef" = "grey80")) +
+  scale_x_datetime(date_labels = as.character(c(0:15)), date_breaks = "1 days", 
+                   limits = plot_time) +
+  theme(panel.background = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_text(size = 10,
+                                 colour = "grey65"),
+        axis.title.y = element_text(size = 12, 
+                                    colour = "grey65"),
+        axis.title.x =  element_text(size = 12, 
+                                     colour = "grey65"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey90"),
+        legend.key = element_blank(),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 12)) 
+
+# save plot
+ggsave("./figs/temp_plot.pdf",
+       width = 10,
+       height = 6.5,
+       units = "in")
+
 # PSII FvFm plot ---------------------------------------------------------------
 library(tidyverse)
 
@@ -30,7 +70,7 @@ ggplot(sum_pamdata, aes(x = day,
   scale_x_discrete(name = "Time (days)",
                    labels = c(1:15)) +
   scale_y_continuous(name = expression(Quantum~Yield~PSII~(F[v]/F[m]))) +
-  scale_colour_manual(values = gordon01::qual_colours, 
+  scale_colour_manual(values = gordon01::qual_colours[c(2,1)], 
                       name = NULL,
                       labels = c("control", "heated")) +
   scale_shape_manual(values = c(16, 17),
