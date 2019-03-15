4c7a327f11d79634a898589985e8825b0999ebeb
Benjamin R. Gordon <brgordon17@gmail.com>
2019-03-15 21:14:55 +1000

    Rerun the model with mzdata with 6000 vars. changed test data to use only 10% of the data.

--- R/mzrf.R
+++ R/mzrf.R
@@ -14,7 +14,7 @@ mzdata <- data.frame(droplevels(mzdata))
 # Partition data into training, test and validation sets
 set.seed(1978)
 train_index <- createDataPartition(mzdata$FvFm,
-                                   p = 0.8,
+                                   p = 0.9,
                                    list = FALSE)
 train_data <- mzdata[train_index, ]
 test_data  <- mzdata[-train_index, ]
@@ -30,7 +30,7 @@ test_data  <- mzdata[-train_index, ]
 # Create model for FvFm prediction ---------------------------------------------
 # tuning grid p/3 (used for regression)
 tunegrid <- expand.grid(.mtry = c(25, 50, 75, 100, 200, 300, 400, 500, 1000,
-                                  floor(length(train_data)/3)))
+                                  max(floor(ncol(mzdata)/3))))
 
 # set seeds
 set.seed(1978)
@@ -50,7 +50,7 @@ ctrl <- caret::trainControl(method = "repeatedcv",
 # model
 doMC::registerDoMC()
 set.seed(1978)
-mzrf_fvfm <- caret::train(x = train_data[, -1:-6],
+mzrf_fvfm <- caret::train(x = train_data[-1:-6],
                      y = train_data$FvFm,
                      method = "rf",
                      trControl = ctrl,
@@ -80,53 +80,54 @@ preds <- bind_cols(id = test_data$sample_id,
 # save as temporary file
 saveRDS(mzrf_fvfm, "./dev/mzrf_model_fvfm.rds")
 
+# NOT RUN ----------------------------------------------------------------------
 # model for cont_treat classification ------------------------------------------
 # tuning grid sqrt(p) (used for classification)
-tunegrid <- expand.grid(.mtry = c(25, 50, 75, 100))
-
-# set seeds
-set.seed(1978)
-seeds <- vector(mode = "list", length = 31)
-for(i in 1:30) seeds[[i]] <- sample.int(1000, length(tunegrid[,1]))
-seeds[[31]] <- sample.int(1000, 1)
-
-# train control
-ctrl <- caret::trainControl(method = "repeatedcv",
-                            number = 10,
-                            repeats = 3,
-                            summaryFunction = defaultSummary,
-                            seeds = seeds,
-                            savePredictions = "all",
-                            classProbs = TRUE,
-                            selectionFunction = "oneSE")
-
-# model
-doMC::registerDoMC()
-set.seed(1978)
-mzrf_cvst <- caret::train(x = train_data[, -1:-6],
-                          y = train_data$cont_treat,
-                          method = "rf",
-                          trControl = ctrl,
-                          #preProc = c("center", "scale"),
-                          allowParallel = TRUE,
-                          importance = TRUE,
-                          tuneGrid = tunegrid
-                          )
-# email notification
-use_secret_file("~/Documents/R/R_emails/R-emails.json")
-send_message(mime(To = "benjamin.gordon@my.jcu.edu.au",
-                  From = "brgordon17@gmail.com",
-                  Subject = "Analysis complete",
-                  body = "Your R process has finished."))
-
-# predict test data
-test_pred <- predict(mzrf_cvst, newdata = test_data)
-
-# test data prediction metrics
-confusionMatrix(test_pred, test_data$cont_treat)
-
-# AUC-ROC for the model
-twoClassSummary(filter(mzrf_cvst$pred, mtry == 50), lev = c("C", "T"))
-
-# save as temporary file
-saveRDS(mzrf_cvst, "./dev/mzrf_model_cvst.rds")
+# tunegrid <- expand.grid(.mtry = c(25, 50, 75, 100))
+# 
+# # set seeds
+# set.seed(1978)
+# seeds <- vector(mode = "list", length = 31)
+# for(i in 1:30) seeds[[i]] <- sample.int(1000, length(tunegrid[,1]))
+# seeds[[31]] <- sample.int(1000, 1)
+# 
+# # train control
+# ctrl <- caret::trainControl(method = "repeatedcv",
+#                             number = 10,
+#                             repeats = 3,
+#                             summaryFunction = defaultSummary,
+#                             seeds = seeds,
+#                             savePredictions = "all",
+#                             classProbs = TRUE,
+#                             selectionFunction = "oneSE")
+# 
+# # model
+# doMC::registerDoMC()
+# set.seed(1978)
+# mzrf_cvst <- caret::train(x = train_data[, -1:-6],
+#                           y = train_data$cont_treat,
+#                           method = "rf",
+#                           trControl = ctrl,
+#                           #preProc = c("center", "scale"),
+#                           allowParallel = TRUE,
+#                           importance = TRUE,
+#                           tuneGrid = tunegrid
+#                           )
+# # email notification
+# use_secret_file("~/Documents/R/R_emails/R-emails.json")
+# send_message(mime(To = "benjamin.gordon@my.jcu.edu.au",
+#                   From = "brgordon17@gmail.com",
+#                   Subject = "Analysis complete",
+#                   body = "Your R process has finished."))
+# 
+# # predict test data
+# test_pred <- predict(mzrf_cvst, newdata = test_data)
+# 
+# # test data prediction metrics
+# confusionMatrix(test_pred, test_data$cont_treat)
+# 
+# # AUC-ROC for the model
+# twoClassSummary(filter(mzrf_cvst$pred, mtry == 50), lev = c("C", "T"))
+# 
+# # save as temporary file
+# saveRDS(mzrf_cvst, "./dev/mzrf_model_cvst.rds")
