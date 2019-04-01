ba5f140c4812096e8d97142f29635b210e86bfdc
Benjamin R. Gordon <brgordon17@gmail.com>
2019-04-01 23:57:35 +1000

    added code to remove day 8 samples from predictions to assess any improvement

--- R/thesis_tables.R
+++ R/thesis_tables.R
@@ -44,6 +44,12 @@ postResample(pred = test_pred, obs = test_data$FvFm)
 # Compare pred vs actual
 preds <- bind_cols(day = test_data$day,
                    treatment = test_data$cont_treat,
-                   actual = test_data$FvFm,
-                   pred = test_pred
+                   actual = round(test_data$FvFm, 3),
+                   predicted = round(test_pred, 3)
                    )
+readr::write_csv(preds, "./tables/unseen_predictions.txt")
+
+# remove day samples from predictions
+test_data_no8 <- filter(test_data, day != "day 8")
+test_pred_no8 <- predict(reg_model, newdata = test_data_no8)
+postResample(pred = test_pred_no8, obs = test_data_no8$FvFm)
