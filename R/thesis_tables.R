824e8fdcc2c57d078b1225dabefb48e1803fbc59
Benjamin R. Gordon <brgordon17@gmail.com>
2019-04-01 23:37:53 +1000

    added table for unseen sample predictions

--- R/thesis_tables.R
+++ R/thesis_tables.R
@@ -2,6 +2,7 @@
 # Author: Benjamin R. Gordon
 # Date: 2019-03-17
 
+# Table of compounds matching literature ---------------------------------------
 library(tidyverse)
 
 # Load and prep data
@@ -14,5 +15,35 @@ matches <-
   select(-ref)
 
 
-# Save csv ---------------------------------------------------------------------
+# Save csv 
 readr::write_csv(matches, "./tables/important_variable_matches.txt")
+
+# Table of predictions for unseen samples --------------------------------------
+library(tidyverse)
+library(caret)
+
+# Load and prep data
+load("./data/mzdata.rda")
+reg_model <- read_rds("./dev/mzrf_model_fvfm.rds")
+
+# test data
+mzdata <- filter(mzdata, cont_treat != "PBQC")
+mzdata <- data.frame(droplevels(mzdata))
+set.seed(16)
+train_index <- createDataPartition(mzdata$FvFm,
+                                   p = 0.9,
+                                   list = FALSE)
+test_data  <- mzdata[-train_index, ]
+
+# predict test data
+test_pred <- predict(reg_model, newdata = test_data)
+
+# test data prediction metrics
+postResample(pred = test_pred, obs = test_data$FvFm)
+
+# Compare pred vs actual
+preds <- bind_cols(day = test_data$day,
+                   treatment = test_data$cont_treat,
+                   actual = test_data$FvFm,
+                   pred = test_pred
+                   )
