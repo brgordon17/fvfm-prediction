204d547d3bf3173b0a0392bd81a249212eb50f03
Benjamin R. Gordon <brgordon17@gmail.com>
2019-07-15 15:29:52 +1000

    added Tukeys stats for all days

--- R/fvfm_anova.R
+++ R/fvfm_anova.R
@@ -29,8 +29,17 @@ TukeyHSD(pam_aov)
 # pull out control vs treatment for each sampling point
 pam_tukey <- as_tibble(TukeyHSD(pam_aov)$`class:day`, rownames = "interaction")
 pam_tukey <- filter(pam_tukey, interaction == "treatment:day 1-control:day 1" |
+                      interaction == "treatment:day 2-control:day 2" |
+                      interaction == "treatment:day 3-control:day 3" |
+                      interaction == "treatment:day 4-control:day 4" |
                       interaction == "treatment:day 5-control:day 5" |
+                      interaction == "treatment:day 6-control:day 6" |
+                      interaction == "treatment:day 7-control:day 7" |
                       interaction == "treatment:day 8-control:day 8" |
+                      interaction == "treatment:day 9-control:day 9" |
                       interaction == "treatment:day 10-control:day 10" |
+                      interaction == "treatment:day 11-control:day 11" |
                       interaction == "treatment:day 12-control:day 12" |
+                      interaction == "treatment:day 13-control:day 13" |
+                      interaction == "treatment:day 14-control:day 14" |
                       interaction == "treatment:day 15-control:day 15")
