43e24020e9b43f58843a29c638965c12b008155f
Benjamin R. Gordon <brgordon17@gmail.com>
2019-11-04 13:25:22 +1000

    initial commit

---
+++ R/figure_pca_raw.R
@@ -0,0 +1,9 @@
+# Code to produce a pca plot of the raw mzdata
+# Author: Benjamin R. Gordon
+# Date: 2019-11-04
+
+# Load libraries and data ------------------------------------------------------
+library(tidyverse)
+
+load("./data/mzdata.rda")
+
